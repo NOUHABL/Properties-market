e9cdd9951e220efaf68d3babd0f8f6968716e4d7
NOUHA <nouhabl2006@gmail.com>
2025-07-12 00:41:02 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import PropertyList from "./PropertyList.css";
 const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+  { id: 129031, name: "Desert Yurt", rating: 4.6, price: 130 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 5, price: 230 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.85, price: 350 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.29, price: 190 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.79, price: 130 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.9, price: 95 },
 ];
 
 function App() {
